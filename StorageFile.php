b57e7cede423b8e5e16d10dd3797fcacca48ace4
AlekseyArh <a@arh.one>
2017-12-07 16:55:52 +0300

    Storage

---
+++ StorageFile.php
@@ -0,0 +1,90 @@
+<?php declare(strict_types = 1);
+namespace arhone\storage;
+
+/**
+ * Хранилище данных (ключ - значение) (PHP 7)
+ *
+ * Class Storage
+ * @package arhone\storage
+ */
+class Storage implements StorageInterface {
+
+    /**
+     * Настройки класса
+     *
+     * @var array
+     */
+    protected $config = [
+        'file' => __DIR__ . '/storage/storage.s'
+    ];
+
+    /**
+     * CacheFile constructor.
+     * @param array $config
+     */
+    public function __construct (array $config = []) {
+
+        $this->config($config);
+
+    }
+
+    /**
+     * Возвращает значение файла
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function get (string $key) {
+
+
+    }
+
+    /**
+     * Записывает значение в файл
+     *
+     * @param string $key
+     * @param $data
+     * @param int|null $interval
+     * @return bool
+     */
+    public function set (string $key, $data, int $interval = null) : bool {
+
+
+    }
+
+    /**
+     * Удаление значения
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function delete (string $key) : bool {
+
+
+    }
+
+    /**
+     * Проверка ключа
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function has (string $key) : bool {
+
+
+    }
+
+    /**
+     * Задаёт конфигурацию
+     *
+     * @param array $config
+     * @return array
+     */
+    public function config (array $config) : array {
+
+        return $this->config = array_merge($this->config, $config);
+
+    }
+
+
+}
---
+++ StorageInterface.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types = 1);
+namespace arhone\storage;
+
+/**
+ * Хранилище данных (ключ - значение) (PHP 7)
+ *
+ * Interface StorageInterface
+ * @package arhone\storage
+ */
+interface StorageInterface {
+
+    /**
+     * Записывает значение в файл
+     *
+     * @param string $key
+     * @param $data
+     * @param int|null $interval
+     * @return bool
+     */
+    public function set (string $key, $data, int $interval = null) : bool ;
+
+    /**
+     * Возвращает значение файла
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function get (string $key);
+
+    /**
+     * Проверяет существует ли ключ
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public function has (string $key);
+
+    /**
+     * Удаление значения
+     *
+     * @param string $key
+     * @return bool
+     */
+    public function delete (string $key) : bool ;
+
+    /**
+     * Задаёт конфигурацию
+     *
+     * @param array $config
+     * @return array
+     */
+    public function config (array $config) : array;
+
+}
